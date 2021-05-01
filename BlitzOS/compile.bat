ef5ef96fe527cca371e40690f632a0e1148a67e4
BlitzWolfMatthew <63450052+BlitzWolfMatthew@users.noreply.github.com>
2021-05-01 13:20:15 +0300

    Delete BlitzOS directory

--- BlitzOS/compile.bat
+++
@@ -1,7 +0,0 @@
-nasm -f elf boot.asm -o boot.o
-g++ -c kernel.cpp -o kernel.o -ffreestanding -fno-exceptions -fno-rtti
-
-ld -T linker.ld -o kernel boot.o kernel.o -build-id=none
-objcopy -O elf32-i386 kernel kernel.elf
-
-qemu-system-i386 -kernel kernel.elf
